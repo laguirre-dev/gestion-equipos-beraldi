ddacc49f93af94e00015735f16c4ed3f4c39887b
laguirre-dev <luciano.aguirre919@gmail.com>
2025-10-24 03:59:12 -0300

    a

--- conexiones.py
+++ conexiones.py
@@ -1,17 +1,34 @@
 import os
 import streamlit as st
 # from dotenv import load_dotenv
-import pyodbc
+# import pyodbc
+import pymssql
 
 # load_dotenv()
 
 def connect_softland():
-    conn = pyodbc.connect(
-        "DRIVER={SQL Server};"
-        f"SERVER={st.secrets['SOFTLAND_DB_SERVER']};"
-        f"DATABASE={st.secrets['SOFTLAND_DB_NAME']};"
-        f"UID={st.secrets['BERALDI_PYTHON_DB_USER']};"
-        f"PWD={st.secrets['BERALDI_PYTHON_DB_PASSWORD']}"
+    server = st.secrets["DB_SERVER"]
+    user = st.secrets["DB_USER"]
+    password = st.secrets["DB_PASSWORD"]
+    database = st.secrets["DB_NAME"]
+
+    conn = pymssql.connect(
+        server=server,
+        user=user,
+        password=password,
+        database=database,
+        login_timeout=10,
+        charset='UTF-8'
     )
-    cursor = conn.cursor()
-    return conn, cursor
+    return conn, conn.cursor()
+
+# def connect_softland():
+#     conn = pyodbc.connect(
+#         "DRIVER={SQL Server};"
+#         f"SERVER={st.secrets['SOFTLAND_DB_SERVER']};"
+#         f"DATABASE={st.secrets['SOFTLAND_DB_NAME']};"
+#         f"UID={st.secrets['BERALDI_PYTHON_DB_USER']};"
+#         f"PWD={st.secrets['BERALDI_PYTHON_DB_PASSWORD']}"
+#     )
+#     cursor = conn.cursor()
+#     return conn, cursor
