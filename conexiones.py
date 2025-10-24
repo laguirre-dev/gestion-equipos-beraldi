b4e0d257a841cdb075b3ee7f1aab18f07cba2403
laguirre-dev <luciano.aguirre919@gmail.com>
2025-10-24 03:38:48 -0300

    Agregado de secret keys Streamlit

--- conexiones.py
+++ conexiones.py
@@ -1,16 +1,17 @@
 import os
-from dotenv import load_dotenv
+import streamlit as st
+# from dotenv import load_dotenv
 import pyodbc
 
-load_dotenv()
+# load_dotenv()
 
 def connect_softland():
     conn = pyodbc.connect(
         "DRIVER={SQL Server};"
-        f"SERVER={os.getenv('SOFTLAND_DB_SERVER')};"
-        f"DATABASE={os.getenv('SOFTLAND_DB_NAME')};"
-        f"UID={os.getenv('BERALDI_PYTHON_DB_USER')};"
-        f"PWD={os.getenv('BERALDI_PYTHON_DB_PASSWORD')}"
+        f"SERVER={st.secrets['SOFTLAND_DB_SERVER']};"
+        f"DATABASE={st.secrets['SOFTLAND_DB_NAME']};"
+        f"UID={st.secrets['BERALDI_PYTHON_DB_USER']};"
+        f"PWD={st.secrets['BERALDI_PYTHON_DB_PASSWORD']}"
     )
     cursor = conn.cursor()
     return conn, cursor
